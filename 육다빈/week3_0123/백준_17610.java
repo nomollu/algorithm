aba21c086e63af704cfbf0f0012b07176bc39c27
Dabisix <80896077+Dabisix@users.noreply.github.com>
2023-02-07 15:31:40 +0900

    [백준] 17610 양팔저울

--- 육다빈/week3_0123/백준_17610.java
+++ 육다빈/week3_0123/백준_17610.java
@@ -3,11 +3,10 @@ package silver;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
+import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.Set;
+import java.util.List;
 import java.util.StringTokenizer;
-import java.util.TreeSet;
 
 public class S17610_scales {
 
@@ -16,35 +15,34 @@ public class S17610_scales {
 		
 		int N = Integer.parseInt(br.readLine());
 		
-		Set<Long> set = new TreeSet<Long>();
-		set.add((long) 0);
-		int sum = 0;
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
+		int[] weight = new int[N];
+		List<Integer> list = new ArrayList<Integer>();
+		list.add(0);
+		int S = 0;
 		for(int i=0; i<N; i++) {
-			int w = Integer.parseInt(st.nextToken());
-			sum += w;
-
-			Iterator<Long> it = set.iterator();
-			Set<Long> tmpSet = new HashSet<Long>();
-			
-			while(it.hasNext()) {
-				long now = it.next();
-				tmpSet.add(now + w);
-				tmpSet.add(now - w);
+			weight[i] = Integer.parseInt(st.nextToken());
+			S += weight[i];
+			int len = list.size();
+			for(int j=0; j<len; j++) {
+				int tmp = list.get(j);
+				list.add(tmp + weight[i]);
+				list.add(tmp - weight[i]);
 			}
-			
-			set.addAll(tmpSet);
-		}
-
-		Iterator<Long> it = set.iterator();
-		while(it.hasNext()) {
-			long now = it.next();
-			if(now>0) break;
-			else it.remove();
 		}
 		
-		System.out.println(sum - set.size());
+		boolean[] visit = new boolean[S+1];
+		for(int num : list) {
+			if(num>0) visit[num] = true;
+		}
+		
+		int cnt = 0;
+		for(int i=1; i<=S; i++) {
+			if(!visit[i]) cnt++;
+		}
+		
+		System.out.println(cnt);
 	}
 
 }
