2c608a9b03bc978877dc17112f032eb3e38c142f
Woo SangWoock <41969902+YeoUlFox@users.noreply.github.com>
2023-02-14 19:23:45 +0900

    Update p_1629.js

--- 우상욱/p_1629.js
+++ 우상욱/p_1629.js
@@ -4,7 +4,6 @@ var [A, B, C] = fs.readFileSync('input.txt').toString().trim().split(' ').map(Bi
 function pow(x, y) {
     if(y == 0) return BigInt(1);
 
-
     let tmp = pow(x, BigInt(parseInt(y / BigInt(2))));
 
     if(y % BigInt(2) == 0) return tmp * tmp % C;
